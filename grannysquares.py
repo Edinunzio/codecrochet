4c5721aaf1b6ce983811c73b1241db849d141177
Edinunzio <e.dinunzio@gmail.com>
2016-04-16 22:06:42 -0400

    Created helper dummy function "get_spaces"
    This will not actually work. It is meant to be an abstraction of the human eye going, oh that is a stitch

--- grannysquares.py
+++ grannysquares.py
@@ -30,6 +30,18 @@ def dc(number_of_stitches):
     """
     return dc * number_of_stitches
 
+
+def get_spaces(row):
+    """Another abstracted function that serves as a "helper".
+    This function only serves to get the sticheable spaces in the row.
+    I am treating a standard round/row in crochet as a list in python.
+    Each stitch is represented as an item in the list.
+    This will not work, and is being used as a way to loop through the row
+    using the computer and not a human's eyes.
+    """
+    spaces = ''  # No need to abstract this out for the overall purpose
+    return spaces
+
 # Round One:
 begin_ring = ch(3)  # Chain 3.
 begin_ring[0].dc(1)  # Dc into first chain.
@@ -46,3 +58,25 @@ begin_ring[0].dc(3)
 ch(2)
 
 begin_ring[2].sl(1)  # Slip stitch in top chain of "chain three" to close round
+
+# Round Two:
+spaces = get_spaces(round_1)
+begin_stitch = spaces[0].ch(3)  # To begin next round, you will chain three
+# This counts as the first dc of the round.
+spaces[0].dc(2)  # Make 2 dc into the same corner as the ch 3.
+
+# In the following corner: 3 dc, 2 ch, 3 dc.
+spaces[1].dc(3)
+spaces[1].ch(2)
+spaces[1].dc(3)  # This makes the new corner.
+
+# Repeating in the next corner.
+spaces[2].dc(3)
+spaces[2].ch(2)
+spaces[2].dc(3)
+
+# In the final corner, you will make 3 dc, chain 2, then slip stitch to the
+# top of first chain in the first "chain three" to close the round
+spaces[2].dc(3)
+spaces[2].ch(2)
+begin_stitch[2].sl(1)
