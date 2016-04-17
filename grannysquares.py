8fae498b553ca8b55180803f0c4bddf4fbe4d802
Edinunzio <e.dinunzio@gmail.com>
2016-04-16 20:56:15 -0400

    Stubbed out functional code to complete "Round One"
    Added obligatory warning to not actually code like this

--- grannysquares.py
+++ grannysquares.py
@@ -1,3 +1,6 @@
+# DO NOT WRITE ACTUAL CODE LIKE THIS. THIS IS FOR EXPERIMENTAL PURPOSES
+
+
 def sl(number_of_stitches):
     """This is an abstraction of a slip stitch.
     For argument's sake, we're assuming all stitch functions will be
@@ -26,3 +29,20 @@ def dc(number_of_stitches):
     within the current space/stitch
     """
     return dc * number_of_stitches
+
+# Round One:
+begin_ring = ch(3)  # Chain 3.
+begin_ring[0].dc(1)  # Dc into first chain.
+begin_ring[0].dc(1)  # Dc again into first chain.
+
+ch(2)  # Chain 2; this create the space that will become your first corner
+begin_ring[0].dc(3)  # 3 dc into the same chain spot.
+ch(2)  # Ch 2 to make your next corner
+
+# Below repeats that previous step twice more
+begin_ring[0].dc(3)
+ch(2)
+begin_ring[0].dc(3)
+ch(2)
+
+begin_ring[2].sl(1)  # Slip stitch in top chain of "chain three" to close round
